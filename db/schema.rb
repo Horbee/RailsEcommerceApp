c09813ee3026267e92f1754b203a22cb4cbfd40b
Horbee <n.horox@gmail.com>
2018-10-23 13:41:12 +0200

    CF 4.2 added price and color fields to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_200554) do
+ActiveRecord::Schema.define(version: 2018_10_23_110603) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_10_21_200554) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.integer "price"
   end
 
 end
