aea4372749611d1f41b7698e323ea97e25ea1053
Horbee <n.horox@gmail.com>
2018-10-24 10:29:46 +0200

    CF 4.3 Creating Orders controller and views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,4 +7,8 @@ class SimplePagesController < ApplicationController
 
   def about
   end
+
+  def landing_page
+    @featured_product = Product.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
-  root 'simple_pages#index'
+  resources :orders, only:[:index, :show, :create, :destroy]
+  # root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/contact'
   get 'simple_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
