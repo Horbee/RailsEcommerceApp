1beae6644500495276dade5bea5604004b65baba
Horbee <n.horox@gmail.com>
2018-10-29 20:41:30 +0100

    CF 4.7 Finale

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,7 +1,4 @@
 class SimplePagesController < ApplicationController
-	layout "different"
-  def index
-  end
 
   def contact
   end
@@ -11,6 +8,5 @@ class SimplePagesController < ApplicationController
 
   def landing_page
     @featured_products = Product.order("id").limit(3)
-    # render layout: "products"
   end
 end
