95e44b912078275594707814b42751c1e37f19e7
Horbee <n.horox@gmail.com>
2018-10-31 22:36:35 +0100

    CF 5.1 Welcome Rerouted

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -9,4 +9,12 @@ class SimplePagesController < ApplicationController
   def landing_page
     @featured_products = Product.order("id").limit(3)
   end
+
+  def redirected
+  	render 'temp/redirected'
+  end
+
+  def alternative
+  	render 'temp/alternative'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  
+  # rerouting the landing page
+  root 'simple_pages#redirected'
+  
+  # the original landin page 
+	get '/landing', to: 'simple_pages#landing_page'
+	get '/alternative', to: 'simple_pages#alternative', as: 'alter'
+  get 'simple_pages/contact'
+  get 'simple_pages/about'
+  
   resources :products
   resources :orders, only:[:index, :show, :create, :destroy]
   # root 'simple_pages#index'
-  root 'simple_pages#landing_page'
-  get 'simple_pages/contact'
-  get 'simple_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
