9f4de3e478185d445893a3064a58978781d5e199
Horbee <n.horox@gmail.com>
2018-11-04 22:58:09 +0100

    CF 5.4 Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+ 
   # rerouting the landing page
   root 'simple_pages#landing_page'
   
@@ -8,8 +9,9 @@ Rails.application.routes.draw do
   
   post 'simple_pages/thank_you'
 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  resources :users
   resources :products
   resources :orders, only:[:index, :show, :create, :destroy]
-  # root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
