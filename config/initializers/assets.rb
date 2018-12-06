5e57c76ae74cf6e11b189dac80e1fdcedcc40c37
Horbee <n.horox@gmail.com>
2018-12-06 14:25:00 -0800

    CF 6.9 ElevateZoom and Redux integrated

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( jquery.raty.js )
-#Rails.application.config.assets.precompile += %w( elevateZoom-308.min.js )
+Rails.application.config.assets.precompile += %w( elevateZoom-308.min.js )
